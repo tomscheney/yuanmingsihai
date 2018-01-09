b5100bd52fb663fcc1bbfffeadb83a131c9f652a
chen_jian <417497299@qq.com>
2018-01-09 23:36:52 +0800

    主页顶部item

--- app.js
+++ app.js
@@ -29,9 +29,9 @@ App({
               var openid = result.openid;
               //指定用户，跳转激活页面
               // if (openid == "odW8G0Yyaew3eZk6SZeYB_uSPKxI") {
-                wx.redirectTo({
-                  url: "/pages/activatedCard/activatedCard",
-                })
+                // wx.redirectTo({
+                //   url: "/pages/activatedCard/activatedCard",
+                // })
               // }
             },
             error: function (error) {
