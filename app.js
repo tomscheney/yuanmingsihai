06b4dc13b36f5d63c142a786e5249a5781dbd55a
Hero <hero@HerodeMacBook-Pro.local>
2018-01-10 13:48:52 +0800

    首页东部视图切换

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ App({
               //指定用户，跳转激活页面
               // if (openid == "odW8G0Yyaew3eZk6SZeYB_uSPKxI") {
 
+                // wx.redirectTo({
+                //   url: "/pages/activatedCard/activatedCard",
+                // })
               // }
 
                if(openid){
--- pages/index/index.js
+++ pages/index/index.js
@@ -6,12 +6,11 @@ const app = getApp()
 Page({
   data: {
     
-    pricerGrade:['1000型','2000型','3000型'],
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
     imageUrl: "http://bmob-cdn-15679.b0.upaiyun.com/2017/12/17/e4f99a5c406c23288092d882ef72e2c0.jpg"  ,
-    
     pruductArray: [[{ productName: '普洱茶', productDescription: '真的很好喝' }, { productName: '大红袍', productDescription: '安神补眠，你值得拥有' }], [{ productName: '铁观音', productDescription: '真的很好喝' }, { productName: '竹叶青', productDescription: '安神补眠，你值得拥有' }], [{ productName: '黄山毛峰', productDescription: '真的很好喝' }, { productName: '龙潭飘雪', productDescription: '安神补眠，你值得拥有' }], ],
     amount:0,
+
+    selectIndex:0,
   },
 
   //事件处理函数
@@ -25,10 +24,17 @@ Page({
   },
   onLoad: function (e) {
    
-   console.log("index:",e);
+  
     
   },
 
+  selectPriceGrade:function(e){
+    console.log(e.currentTarget.dataset.index)
+    var index = e.currentTarget.dataset.index;
+    this.setData({
+      selectIndex: index
+    })
+  }
 })
 
 
